27a511c3eeab8107b85571cfba3a931efdd5198f
VenkataKarthikNeeluri <nvenkatakarthik028@gmail.com>
2023-07-18 13:57:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// npm i --save express cors dotenv nodemailer
+
+const express = require('express')
+require('dotenv').config()
+
+const PORT = process.env.PORT 
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(`/`, require('./route/mailRoute'))
+
+app.listen(PORT, () => {
+    console.log(`server is started @ http://localhost:${PORT}`)
+})
